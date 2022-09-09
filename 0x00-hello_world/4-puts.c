217d389bc1141f560847ae8399231d9cd0757be1
kaysiialexandre <kaysiialexandre@gmail.com>
2022-09-08 17:48:43 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,16 +3,15 @@
 /**
  * main - Entry point
  *
- * return: always 0
+ * Return: always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	
+	puts("\"Programming is like building a multilingual puzzle");
 	/**
-	 * returnvalue - return value for main
+	 * return value - return value for main
 	 *
-	 * return: always 0
+	 * Return: always 0
 	 */
-	return(0);
+	return (0);
 }
