3cd39803ae4e721431ac6de5a34d967c4377e80a
kaysiialexandre <kaysiialexandre@gmail.com>
2022-09-08 17:15:49 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: always 0 (succes)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
+}
