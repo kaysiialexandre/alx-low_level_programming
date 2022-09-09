38363f46f4a1e2ec2f9acc6040bca79883cc88fd
kaysiialexandre <kaysiialexandre@gmail.com>
2022-09-08 19:08:50 -0700

    size of types in C programming language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("The size of a char is: %ld bytes\n", sizeof(char));
-	printf("The size of an int is: %ld bytes\n", sizeof(int));
-	printf("The size of a long int is: %ld bytes\n", sizeof(long int));
-	printf("Size of a long long int is: %ld bytes\n", sizeof(long long int));
-	printf("The size of float is: %ld bytes\n", sizeof(float));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld bytes\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
