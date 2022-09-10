5446714543ea24940f9a509477c5de8a18a4ab75
kaysiialexandre <kaysiialexandre@gmail.com>
2022-09-09 18:13:18 -0700

    decremental operand applied

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char r;
+
+	for (r = 'z'; r >= 'a'; r--)
+	{
+		putchar(r);
+	}
+	putchar('\n');
+	return (0);
+}
