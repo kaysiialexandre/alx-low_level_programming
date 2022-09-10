8b044f6d694f0a5d365827e77c6c861f03b461f9
kaysiialexandre <kaysiialexandre@gmail.com>
2022-09-09 20:52:23 -0700

    combinations of three different digits using putchar

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,7 +20,7 @@ int main(void)
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
 				putchar((k % 10) + '0');
-				if (i <= 7 || j <= 8 || k <= 9)
+				if (i < 7 || j < 8 || k < 9)
 				{
 					putchar(',');
 					putchar(' ');
