e11c8cb667bca049ccfff4a83173b92ec816d155
kaysiialexandre <kaysiialexandre@gmail.com>
2022-09-19 17:31:03 -0700

    reversedprint function

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,9 @@
  */
 void print_rev(char *s)
 {
-	int i;
-	    
-	    for (i = _strlen(s) - 1; i >= 0; i--)
+	int i
+
+	for (i = _strlen(s) - 1; i >= 0; i--)
 		_putchar(*(s + i));
 	_putchar('\n');
 }
