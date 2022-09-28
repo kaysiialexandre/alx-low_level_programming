6ff26b777ae39404e1afa0ea4e79a6b9fb3f793b
kaysiialexandre <kaysiialexandre@gmail.com>
2022-09-28 14:44:59 -0700

    is_palindrome function added

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,8 @@ int _pow_recursion(int x, int y);
 int helper(int i, int n);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int length(char *s);
+int helper2(int i, char *s);
+int is_palindrome(char *s);
 
 #endif /* MAIN_H */
