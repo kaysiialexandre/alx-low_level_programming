23fd65a75d9443e9bb0c84ced6acc9f88e004a06
kaysiialexandre <kaysiialexandre@gmail.com>
2022-10-06 14:25:22 -0700

    _realloc function added

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,6 @@ int _putchr(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /* MAIN_H */
